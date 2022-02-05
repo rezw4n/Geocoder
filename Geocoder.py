b7fb09ca7a7d08d322a9486d2f06bcb88e006092
Rezwan Ahmed <rezwan490@gmail.com>
2022-02-05 18:46:43 +0600

    Initial commit

---
+++ Geocoder.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import streamlit as st
+from stqdm import stqdm
+
+
+def geocode(address, country='', city=''):
+    api = f"https://nominatim.openstreetmap.org/search.php?q={address}&country={country}&city={city}&format=jsonv2"
+    response = requests.get(api).json()[0]
+    return [float(response['lat']), float(response['lon'])]
+
+
+st.title('Geocoder')
+file = st.file_uploader('Upload a csv file:', type='csv')
+
+if file:
+    df = pd.read_csv(file)
+    st.write(df)
+    address = st.selectbox("Select a column you'd like to geocode",
+                           options=["<select>", *df.columns])
+
+    if address != "<select>":
+        stqdm.pandas()
+        df['Latitude and Longitude'] = df[address].progress_apply(lambda x: geocode(x))
+        df['latitude'] = df['Latitude and Longitude'].apply(lambda x: x[0])
+        df['longitude'] = df['Latitude and Longitude'].apply(lambda x: x[1])
+        df.drop('Latitude and Longitude', axis=1, inplace=True)
+        st.write(df)
